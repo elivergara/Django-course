2329f96388149b0afa798aa8d035cd05429ecd9a
Eli Vergara <112268813+elivergara@users.noreply.github.com>
2025-01-10 16:51:56 -0700

    added tests, did not work :(

--- mynewproject/myapp/tests.py
+++ mynewproject/myapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from datetime import datetime
+from .models import Reservation
 # Create your tests here.
+
+class ReservationModelTest(TestCase):
+
+    def setUpTestData(cls):
+        cls.reservation = Reservation.objects.create(
+            name='Eli Vergara',
+            contact='520-280-9601',
+
+        )
+
+def test_fields(self):
+    self.assertIsInstance(self.reservation.name,  str)
+    self.assertIsInstance(self.reservation.contact, str)
+
+def test_timestamps(self):
+    self.assertIsInstance(self.reservation.time, datetime)
